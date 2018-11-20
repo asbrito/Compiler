43439ad2994ae409f35e051cd0c5e6c659576832
srosanatalia <srosanatalia@gmail.com>
2018-11-20 00:17:02 -0300

    Projeto

--- src/br/uefs/ecomp/Compilador/model/Parsing.java
+++ src/br/uefs/ecomp/Compilador/model/Parsing.java
@@ -18,14 +18,27 @@ public class Parsing {
         this.tokenList = tokenList;
     }
     
-    public void controllerParsing(int i){
+    public int controllerParsing(int i){
         Type type;
         switch(((Token)tokenList.get(i)).getType()){
-
+            case Keyword:
+                switch (((Token)tokenList.get(i)).getLexeme()){
+                    case "if":
+                        i = ifelseStructure (i);
+                        return i;
+                    case "while":
+                        break;
+                    case "write":
+                        break;
+                    case "read":
+                        break;
+                }
+            return i;
         }
+        return i;
     }
     
-    public int ifStructure (int i){ //Recebe posição do token na lista de tokens
+    public int ifelseStructure (int i){ //Recebe posição do token na lista de tokens
         Stack ifStructureStack = new Stack ();
         ifStructureStack.add(((Token)tokenList.get(i)));
         i++;
@@ -46,13 +59,7 @@ public class Parsing {
                         ifStructureStack.add(((Token)tokenList.get(i)));
                         i++;
                         while (!("}".equals(((Token)tokenList.get(i)).getLexeme()))){
-                            switch(((Token)tokenList.get(i)).getLexeme()){
-                                case "if":
-                                    int structureValidation = ifStructure (i);
-                                    if (structureValidation == 0){System.out.println("Erro em if!");}
-                                break;
-                            }
-                            i++;
+                            i = controllerParsing(i);
                         }
                         if ("}".equals(((Token)tokenList.get(i)).getLexeme())){ 
                             ifStructureStack.pop(); //Desempilha '}'
@@ -63,55 +70,35 @@ public class Parsing {
                 }
             }
         }
+        
         if (ifStructureStack.isEmpty()){
-            
-            if ("else".equals(((Token)tokenList.get(i)).getLexeme())){ 
-                int structureCall = elseStructure(i);
-                i = structureCall;
-            }
-            
             System.out.println("SUCESSO em if.");
+            if (!(tokenList.isEmpty())){
+                if("else".equals(((Token)tokenList.get(i)).getLexeme())){
+                    Stack elseStructureStack = new Stack ();
+                    elseStructureStack.add(((Token)tokenList.get(i)));
+                    i++;
+                    if ("{".equals(((Token)tokenList.get(i)).getLexeme())){ 
+                        elseStructureStack.add(((Token)tokenList.get(i)));
+                        i++;
+                        while (!("}".equals(((Token)tokenList.get(i)).getLexeme()))){
+                            i++;
+                        }
+
+                        if ("}".equals(((Token)tokenList.get(i)).getLexeme())){ 
+                            elseStructureStack.pop(); //Desempilha '}'
+                            elseStructureStack.pop(); //Desempilha 'else'
+                            i++;
+                        }
+                    }
+                    if (elseStructureStack.isEmpty()){
+                        System.out.println("SUCESSO em else");
+                    }
+                }
+            }
             return i; // retorna lugar que parou
         }
         return 0;
     }
-    public int elseStructure (int i){
-        Stack elseStructureStack = new Stack ();
-        if ("else".equals(((Token)tokenList.get(i)).getLexeme())){ 
-            elseStructureStack.add(((Token)tokenList.get(i)));
-            i++;
-            if ("{".equals(((Token)tokenList.get(i)).getLexeme())){ 
-                elseStructureStack.add(((Token)tokenList.get(i)));
-                i++;
-                while (!("}".equals(((Token)tokenList.get(i)).getLexeme()))){
-                    switch(((Token)tokenList.get(i)).getLexeme()){
-                        case "if":
-                            int structureValidation = ifStructure (i);
-                            i = structureValidation;
-                            //if (structureValidation == 0){System.out.println("Erro em else!");}
-                        break;
-                    }
-                    i++;
-                }
-                
-                if ("}".equals(((Token)tokenList.get(i)).getLexeme())){ 
-                    elseStructureStack.pop(); //Desempilha '}'
-                    elseStructureStack.pop(); //Desempilha 'else'
-                    i++;
-                }
-                
-            }
-            if (elseStructureStack.isEmpty()){
-                System.out.println("SUCESSO em else");
-                return i;
-            }
-        }
-        return 0;
-    }
     
-    public boolean expressionStructure (int i){
-        
-        return false;
-    }
-      
 }
